422a19266c1ee9c33781ccf93d22704557387506
levashov <levashov@samsonpost.ru>
2026-05-23 11:58:05 +0300

    - ошибка типа

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Entity\Appointment;
+use App\Entity\Organization;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
